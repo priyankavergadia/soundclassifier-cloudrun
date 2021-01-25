e58c27b1e3d1fe7fd72ddd756f0d335035258e32
Priyanka Vergadia <priyanka.vergadia@gmail.com>
2021-01-24 18:08:21 -0800

    First push

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require("path");
+const app = express();
+
+//Serve website
+app.use(express.static(path.join(__dirname, "public")));
+
+
+app.get('/', (req, res) => {
+ res.sendFile(path.join(__dirname, "public", "index.html"), err => {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
+
+
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log('Hello world listening on port', port);
+});
